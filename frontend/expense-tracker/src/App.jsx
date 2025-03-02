25544a4798e6bf2e254a64ced0a340b4dbe0e2ad
Muhammad Aswan <muhaswan190897@gmail.com>
2025-03-02 08:33:49 +0800

    feat: implement routing and authentication redirection in App component

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
+
 export const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Root />} />
+					<Route path="/login" exact element={<Login />} />
+					<Route path="/signUp" exact element={<SignUp />} />
+					<Route path="/dashboard" exact element={<Home />} />
+					<Route path="/income" exact element={<Income />} />
+					<Route path="/expense" exact element={<Expense />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 };
 
 export default App;
+
+const Root = () => {
+	// Check if token exists in local storage
+	const isAuthenticated = !!localStorage.getItem('token');
+
+	// Redirect to dashboard if authenticated, otherwise to login
+	return isAuthenticated ? (
+		<Navigate to="/dashboard" />
+	) : (
+		<Navigate to="/login" />
+	);
+};
