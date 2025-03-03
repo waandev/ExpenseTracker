a8ddd076ca7ab9d8eeb38de9788db5289a3a6072
Muhammad Aswan <muhaswan190897@gmail.com>
2025-03-03 10:14:41 +0800

    feat: implement login functionality with email and password validation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,6 +44,27 @@ exports.registerUser = async (req, res) => {
 	}
 };
 
-exports.loginUser = async (req, res) => {};
+exports.loginUser = async (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(400).json({ message: 'All fields are required' });
+	}
+	try {
+		const user = await User.findOne({ email });
+		if (!user || !(await user.comparePassword(password))) {
+			return res.status(400).json({ message: 'Invalid credentials' });
+		}
+
+		res.status(200).json({
+			id: user._id,
+			user,
+			token: generateToken(user._id),
+		});
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: 'Error logging in user', error: error.message });
+	}
+};
 
 exports.getUserInfo = async (req, res) => {};
