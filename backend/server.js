72e71b10ef0a2c2385e65040d6b7d2e436b4006e
Muhammad Aswan <muhaswan190897@gmail.com>
2025-03-03 08:37:00 +0800

    feat: configure MongoDB connection and initialize database connection in server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(
 );
 
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
