82a570cf303d634cc906d7ff07aacd78e9496f4a
Muhammad Aswan <muhaswan190897@gmail.com>
2025-03-04 08:19:02 +0800

    feat: add income management functionality with routes and controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const incomeRoutes = require('./routes/incomeRoutes');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.json());
 connectDB();
 
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/income', incomeRoutes);
 
 // Serve uploaded folder
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
