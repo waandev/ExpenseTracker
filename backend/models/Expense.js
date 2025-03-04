2e0ef91d59ad348d9ecac8008c7f966bb054a077
Muhammad Aswan <muhaswan190897@gmail.com>
2025-03-05 07:39:38 +0800

    feat: add expense management functionality with model, routes, and controller

---
+++ backend/models/Expense.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ExpenseSchema = new mongoose.Schema(
+	{
+		userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+		icon: { type: String },
+		category: { type: String, required: true }, // Example: Food, Rent, Groceries
+		amount: { type: Number, required: true },
+		date: { type: Date, default: Date.now },
+	},
+	{ timestamp: true }
+);
+
+module.exports = mongoose.model('Expense', ExpenseSchema);
--- backend/models/Income.js
+++ backend/models/Income.js
@@ -2,26 +2,11 @@ const mongoose = require('mongoose');
 
 const IncomeSchema = new mongoose.Schema(
 	{
-		userId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-			required: true,
-		},
-		icon: {
-			type: String,
-		},
-		source: {
-			type: String,
-			required: true,
-		}, // Example: Salary, Business, etc.
-		amount: {
-			type: Number,
-			required: true,
-		},
-		date: {
-			type: Date,
-			default: Date.now,
-		},
+		userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+		icon: { type: String },
+		source: { type: String, required: true }, // Example: Salary, Business, etc.
+		amount: { type: Number, required: true },
+		date: { type: Date, default: Date.now },
 	},
 	{ timestamps: true }
 );
