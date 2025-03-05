59152c3b69b2da61742c77399760ccbf28bd5049
Muhammad Aswan <muhaswan190897@gmail.com>
2025-03-05 09:05:07 +0800

    feat: implement user context for authentication and manage user state

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -12,21 +12,24 @@ import SignUp from './pages/Auth/SignUp';
 import Home from './pages/Dashboard/Home';
 import Income from './pages/Dashboard/Income';
 import Expense from './pages/Dashboard/Expense';
+import UserProvider from './context/UserContext';
 
 export const App = () => {
 	return (
-		<div>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Root />} />
-					<Route path="/login" exact element={<Login />} />
-					<Route path="/signUp" exact element={<SignUp />} />
-					<Route path="/dashboard" exact element={<Home />} />
-					<Route path="/income" exact element={<Income />} />
-					<Route path="/expense" exact element={<Expense />} />
-				</Routes>
-			</Router>
-		</div>
+		<UserProvider>
+			<div>
+				<Router>
+					<Routes>
+						<Route path="/" element={<Root />} />
+						<Route path="/login" exact element={<Login />} />
+						<Route path="/signUp" exact element={<SignUp />} />
+						<Route path="/dashboard" exact element={<Home />} />
+						<Route path="/income" exact element={<Income />} />
+						<Route path="/expense" exact element={<Expense />} />
+					</Routes>
+				</Router>
+			</div>
+		</UserProvider>
 	);
 };
 
--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import AuthLayout from '../../components/Layouts/AuthLayout';
 import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from './../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { UserContext } from '../../context/UserContext';
 
 const Login = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [error, setError] = useState(null);
+
+	const { updateUser } = useContext(UserContext);
 
 	const navigate = useNavigate();
 
@@ -28,6 +33,25 @@ const Login = () => {
 		setError('');
 
 		// Login API Call
+		try {
+			const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+				email,
+				password,
+			});
+			const { token, user } = response.data;
+
+			if (token) {
+				localStorage.setItem('token', token);
+				updateUser(user);
+				navigate('/dashboard');
+			}
+		} catch (error) {
+			if (error.response && error.response.data.message) {
+				setError(error.response.data.message);
+			} else {
+				setError('Something went wrong. Please try again.');
+			}
+		}
 	};
 
 	return (
